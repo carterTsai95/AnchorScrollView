fe85054f948e63a956bbcf50fa770bef88765242
Hung-Chun(Carter) Tsai <tsaihong1995@gmail.com>
2022-09-18 13:39:36 -0400

    Initial Commit

---
+++ Shared/AnchorScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnchorScrollViewApp.swift
+//  Shared
+//
+//  Created by Hung-Chun Tsai on 2022-09-18.
+//
+
+import SwiftUI
+
+@main
+struct AnchorScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
